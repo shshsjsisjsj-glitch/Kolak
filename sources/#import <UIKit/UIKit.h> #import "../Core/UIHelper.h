a8ce0919276692d06caf821e04500ab5c2a50b78
shshsjsisjsj-glitch <shshsjsisjsj@gmail.com>
2025-12-09 21:43:15 +0100

    Add UIKit imports and define UI interfaces

---
+++ sources/#import <UIKit/UIKit.h> #import "../Core/UIHelper.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+#import "../sources/satooios.h"
+
+@interface _a9DkPq7LsTv0 : UIView
+@property (nonatomic, strong) NSArray<UIView *> *_xR3mZ28JqU1o;
+@end
+
+@interface Khanhios (UIHelpers)
+- (void)forceCloseApp;
+- (UIViewController *)topViewController;
+@end
